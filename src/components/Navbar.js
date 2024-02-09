42dcc9dd9245905e8ded68a41c8798d5505d3a50
puni7777777 <punithkumar1732003@gmail.com>
2024-02-09 16:23:20 +0530

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,10 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className={'nav-link ' + props.home_active} aria-current="page" href="/">Home</a>
+                            <a className={`nav-link ${props.home_active}`} aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className={'nav-link ' + props.about_active} href="/about">About</a>
+                            <a className={`nav-link ${props.about_active}`} href="/about">About</a>
                         </li>
                     </ul>
                 </div>
