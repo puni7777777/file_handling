82f1475353ba2a0ef2ebaa018f228bff657f34db
puni7777777 <punithkumar1732003@gmail.com>
2024-02-09 19:30:42 +0530

    title change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ zIndex: 50 }}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <a className="navbar-brand">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
