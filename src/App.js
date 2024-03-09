11f6d12d7c34c774bc13b545bbf3ec2c796eabad
puni7777777 <punithkumar1732003@gmail.com>
2024-03-09 16:33:14 +0530

    firebase-trail

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './Firebase'
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
 
-  // const [homeActive, setHomeActive] = useState('active');
-  // const [aboutActive, setAboutActive] = useState('');
+  const [homeActive, setHomeActive] = useState('active');
+  const [aboutActive, setAboutActive] = useState('');
 
-  // const handleHomeClick = () => {
-  //   setHomeActive('active');
-  //   setAboutActive('');
-  //   console.log("home")
-  // };
+  const handleHomeClick = () => {
+    setHomeActive('active');
+    setAboutActive('');
+    console.log("home")
+  };
 
-  // const handleAboutClick = () => {
-  //   setHomeActive('');
-  //   setAboutActive('active');
-  //   console.log("about")
-  // };
+  const handleAboutClick = () => {
+    setHomeActive('');
+    setAboutActive('active');
+    console.log("about")
+  };
 
   return (
     <BrowserRouter>
       <>
-        {/* <Navbar title="TextHandle" home_active={homeActive}
+        <Navbar title="TextHandle" home_active={homeActive}
           about_active={aboutActive}
-          onHomeClick={handleHomeClick} onAboutClick={handleAboutClick} /> */}
+          onHomeClick={handleHomeClick} onAboutClick={handleAboutClick} />
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7mASlUOfEQrfy8uIsEf2sZrzFUOIQL1Y",
+  authDomain: "texthandle-2e7e3.firebaseapp.com",
+  projectId: "texthandle-2e7e3",
+  storageBucket: "texthandle-2e7e3.appspot.com",
+  messagingSenderId: "840809169380",
+  appId: "1:840809169380:web:04e1e3fe31ac9d0a9e87f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
