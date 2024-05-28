e8165314cd3663daffbdc4dbd786b69bde723427
puni7777777 <punithkumar1732003@gmail.com>
2024-05-29 00:19:58 +0530

    error fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a href='texthandle' className={'nav-link'} aria-current="page" >Home</a>
+                                <a href='/' className={'nav-link'} aria-current="page" >Home</a>
                             </li>
                             <li className="nav-item">
                                 <a href='about' className={'nav-link'} >About</a>
@@ -61,9 +61,9 @@ export default function Navbar(props) {
                 </div>
             </nav>
             <Routes>
-                <Route path="texthandle" exact element={<Home />} />
-                <Route path="about" element={<About />} />
-                <Route path="contact" element={<ContactUs />} />
+                <Route path="/" exact element={<Home />} />
+                <Route path="texthandle/about" element={<About />} />
+                <Route path="texthandle/contact" element={<ContactUs />} />
             </Routes>
         </>
     )
